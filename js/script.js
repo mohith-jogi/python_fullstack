85ad80049ac01413623f4404fe2576919a663ac8
mohith-jogi <mohithphanisai@gmail.com>
2026-07-28 10:48:41 +0000

    js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+alert("Welcome to the NRIIT Learning Management System")
+let heading = document.getElementById("welcome");
+heading.innerHTML = "welcome future software engineers"
+console.log("Heading element: ", heading)
---
+++ js/test.js
@@ -0,0 +1,30 @@
+console.log("Hello world")
+let student_name = "Mohith"
+console.log("Student name is: " +student_name)
+let student_age = 21
+let course_name = "Python Full Stack"
+console.log("student age is: "+student_age, "course_name is: " +course_name)
+let fee = 50000
+let discount =20
+let discounted_fee = fee - (fee *discount / 100)
+console.log("Discounted fee is: " +discounted_fee)
+
+let age=18
+if(age>=18){
+    console.log("Eligible for admission")
+}
+else{
+    console.log("Not eligible for admission")
+}
+for(let i=1; i<=5; i++){
+    console.log("Iteration number : " + i)
+}
+const pi = 3.14
+console.log("Value of pi is: " + pi)
+let student = {
+    name: "Mohith",
+    age: 21,
+    course: "Python Full Stack",
+    fee: 10000
+}
+console.log("Student")
