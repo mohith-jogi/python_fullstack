e8e3b682a95bc6fe28bcc9abc60bbc85500b2bbb
mohith-jogi <mohithphanisai@gmail.com>
2026-07-29 10:47:55 +0000

    js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,22 @@ alert("Welcome to the NRIIT Learning Management System")
 let heading = document.getElementById("welcome");
 heading.innerHTML = "welcome future software engineers"
 console.log("Heading element: ", heading)
+let msg = document.getElementById("message")
+msg.innerHTML = "javascript is fun"
+console.log("message element:",msg)
+function showmessge(){
+    alert("welcome to NRIIT learning Management system")}
+function changeHeading(){
+    document.getElementById("welcome").innerHTML ="welcome python Fullstack Developers"} 
+let heading=document.querySelector("#welcome");
+console.log("Heading element: ", heading)
+let button = document.getElementById("btmGreeting");
+button.addEventListener("click", function (){
+    alert("welcome to javascript Eent handling");
+});
+let registerform = document.getElementById("registerfrom");
+function (event) {
+    event.preventDefault():
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email.com")
+}       
